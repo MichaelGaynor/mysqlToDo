29fa6d5c9660a87948340f589a4339c2ff009f74
Michael Gaynor <jmichaelgaynor@gmail.com>
2017-06-06 10:45:30 -0400

    Edit works, code is sanitized, we have ascended

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.get('/', function(req, res, next) {
     var message = req.query.msg;
     if (message == "added"){
         message = "Your task was added!";
+    } else if (message == "edited"){
+        message = "Your task was edited!"
     }
     var selectQuery = "SELECT * FROM tasks ORDER BY taskDate ASC";
     connection.query(selectQuery, (error,results)=>{
@@ -30,18 +32,42 @@ router.post('/addItem', function(req, res){
     // res.json(req.body);
     var newTask = req.body.newTask;
     var dueDate = req.body.newTaskDate;
-    var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES ('"+newTask+"','"+dueDate+"')";
+    var insertQuery = "INSERT INTO tasks (taskName, taskDate) VALUES (?,?)";
     // res.send(insertQuery);
-    connection.query(insertQuery, (error, results)=>{
+    connection.query(insertQuery, [newTask,dueDate], (error, results)=>{
         if(error) throw error;
         res.redirect('/?msg=added');
     })
 });
+
+router.get('/edit/:id', function(req, res){
+    var idToEdit = req.params.id;
+    var selectQuery = "SELECT * FROM tasks WHERE id = ?";
+    connection.query(selectQuery, [idToEdit], (error,results)=>{
+        if (error) throw error;
+        res.render('edit',{
+            task: results[0]
+        });
+    });
+});
+
+router.post('/editItem', (req,res)=>{
+    // res.json(req.body);
+    var newTask = req.body.newTask;
+    var taskDate = req.body.newTaskDate;
+    var taskId = req.query.id;
+    var updateQuery = "UPDATE tasks SET taskName = ?, taskDate = ? WHERE id = ?";
+    connection.query(updateQuery, [newTask,taskDate,taskId], (error, results)=>{
+        if (error) throw error;
+        res.redirect('/?msg=edited')
+    })
+});
+
 router.get('/delete/:id', function(req,res){
     var idToDelete = req.params.id;
-    var deleteQuery = "DELETE FROM tasks WHERE id = "+idToDelete;
+    var deleteQuery = "DELETE FROM tasks WHERE id = ?";
     // res.send(idToDelete);
-    connection.query(deleteQuery, (error, results)=>{
+    connection.query(deleteQuery, [idToDelete], (error, results)=>{
         if (error) throw error;
         res.redirect('/?msg=deleted')
     });
